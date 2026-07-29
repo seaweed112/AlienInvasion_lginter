456851a86615dd3ac72be4798fbabfd5ed0462a9
seaweed112 <your-email@example.com>
2026-07-29 19:21:27 -0400

    Add logic to let ship fire horizontally

--- bullet.py
+++ bullet.py
@@ -8,21 +8,26 @@ Initial repo forked from RedBeard41
 import pygame
 from pygame.sprite import Sprite
 class Bullet(Sprite):
+    """Class to manage bullets fired from the player's ship"""
     def __init__(self, ai_game):
+        """Create the bullet at the front of the ship"""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
-
-        self.rect = pygame.Rect(0,
-                                0,
-                                self.settings.bullet_width,
-                                self.settings.bullet_height)
+        #Create bullet
+        self.rect = pygame.Rect(
+            0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        #Set it to the ships midleft
+        self.rect.midleft = ai_game.ship.rect.midleft
         
-        self.rect.midtop = ai_game.ship.rect.midtop
+        #Store position as a float for the horizontal movement
+        self.x = float(self.rect.x)
 
     def update(self): 
-        self.rect.y -= self.settings.bullet_speed
+        """Move the bullet left across the screen"""
+        self.x -= self.settings.bullet_speed
+        self.rect.x = self.x
 
     def draw_bullet(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ class Settings:
         self.ship_speed = 3.0
 
         self.bullet_speed = 5.0
-        self.bullet_width = 5
-        self.bullet_height = 15
+        self.bullet_width = 15
+        self.bullet_height = 5
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 6
 
