dfc8a5d696c8490d42c71ebe53977e8e2eb4b4cd
seaweed112 <your-email@example.com>
2026-07-29 19:47:20 -0400

    Add documentation to game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -1,7 +1,17 @@
+"""
+game_stats.py
+Lincoln Ginter
+Alien Invasion Arcade Game - (Track 1)
+7/28/2026
+Initial repo forked from RedBeard41
+"""
 class GameStats:
+    """Class to track game stats"""
     def __init__(self, ai_game):
+        """Initialize stats"""
         self.settings = ai_game.settings
         self.reset_stats()
 
     def reset_stats(self):
+        """Begin tracking the amount of lives the player has left"""
         self.ships_left = self.settings.ship_lives
