9094245cbcec02535650db202d0d75dd583e7f1a
seaweed112 <your-email@example.com>
2026-07-29 19:48:45 -0400

    Add missing documentation to draw_bullet

--- bullet.py
+++ bullet.py
@@ -30,4 +30,5 @@ class Bullet(Sprite):
         self.rect.x = self.x
 
     def draw_bullet(self):
+        """Draw the bullet on the screen"""
         pygame.draw.rect(self.screen, self.color, self.rect)
