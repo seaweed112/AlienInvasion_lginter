0d7a6d691cd42639f63b520042a19b9301be8515
seaweed112 <your-email@example.com>
2026-08-04 20:34:47 -0400

    Fix scoreboard typo

--- scoreboard.py
+++ scoreboard.py
@@ -45,7 +45,7 @@ class Scoreboard:
 
     def prep_high_score(self):
         """Render the high score"""
-        high_Score = round(self.stats.high_score, -1)
+        high_score = round(self.stats.high_score, -1)
         high_score_str = f"{self.stats.high_score:,}"
         self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
